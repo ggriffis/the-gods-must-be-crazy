a8ef23972b39d6d93077338ebbc6bfd957c4e5cb
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-29 14:20:00 -0700

    trying to get rid of old files I deleted

--- db/migrate/20130729192628_add_reference_to_human_from_guardian_angel.rb
+++
@@ -1,7 +0,0 @@
-class AddReferenceToHumanFromGuardianAngel < ActiveRecord::Migration
-  def change
-    change_table :guardian_angels do |t|
-      t.references :human
-    end
-  end
-end
--- spec/controllers/humans_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe HumansController do
-
-end
