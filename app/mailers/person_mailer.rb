5d540434e8de34f7925bcf3016f3d93ddd5b3957
Ginger Griffis <gingergriffis@hotmail.com>
2013-08-08 14:09:18 -0700

    Fixed reply to to add plus sign

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -4,7 +4,7 @@ class PersonMailer < ActionMailer::Base
   def welcome_email(person)
     @person = person
     @url  = god_guardian_angel_url(@person.guardian_angel.god, @person.guardian_angel)
-    reply_to = "172c860d9f1b5ef4072e673dff1d7b05" +
+    reply_to = "172c860d9f1b5ef4072e673dff1d7b05" + "+" +
                 @person.id.to_s + "@inbound.postmarkapp.com"
     mail(to: @person.email, subject: 'Welcome to the Physical World',
           reply_to: reply_to)
